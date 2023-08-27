7773126288919a01107c5e522fbc209d98907117
rn25-long-app <bhlong3112@gmail.com>
2023-08-27 14:04:50 +0700

    edit API service & bo sungPhan API post cua bai truoc

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -98,24 +98,26 @@ let createNewUser = (data) => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message: "Your email is already in use, Please try another email !!",
+          errMessage:
+            "Your email is already in use, Please try another email !!",
+        });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.passWord);
+        await db.User.create({
+          email: data.email,
+          passWord: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
+        });
+        resolve({
+          errCode: 0,
+          message: "OK",
         });
       }
-      let hashPasswordFromBcrypt = await hashUserPassword(data.passWord);
-      await db.User.create({
-        email: data.email,
-        passWord: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId,
-      });
-      resolve({
-        errCode: 0,
-        message: "OK",
-      });
     } catch (e) {
       reject(e);
     }
