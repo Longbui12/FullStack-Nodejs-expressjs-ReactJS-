e7d94566a861418c4c3fd75167c4d0564a639877
rn25-long-app <bhlong3112@gmail.com>
2023-11-06 01:34:41 +0700

    update

--- src/services/ClinicService.js
+++ src/services/ClinicService.js
@@ -40,7 +40,7 @@ let getAllClinicService = () => {
       let data = await db.clinic.findAll({});
       if (data && data.length > 0) {
         data.map((item) => {
-          item.image = new Buffer(item.image, "base64").toString("binary");
+          item.image = Buffer.from(item.image, "base64").toString("binary");
           return item;
         });
       }
